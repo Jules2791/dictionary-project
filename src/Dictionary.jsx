67519ec73704f40537216b336958bff7612280f4
Jules2791 <may.julie@yahoo.co.uk>
2025-11-06 07:51:39 +0000

    Added API Documentation

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -12,6 +12,7 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
+    //Api Documentation https://www.shecodes.io/learn/apis/dictionary
     let apiKey = "ba5f0o41c7ff3cc032eee04b3d66at0a";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
